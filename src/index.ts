5218086884713e8e72def17d20b6cce471391ac4
Brandon Slade <brandonyoyoslade@gmail.com>
2020-07-30 13:30:43 -0500

    feat(*): add rotating proxy and failure backoff

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import axios from 'axios';
+import createHttpsProxyAgent = require('https-proxy-agent');
 import * as domino from 'domino';
 import { pledgeConfig, PledgeConfig } from './config';
 
-const SLEEP_MILLIS=1000*60*5; // Every five minutes
+const SLEEP_MILLIS=1000*60*3; // Every three minutes
+const BACKOFF_SLEEP_MILLIS=1000*60*15; // Every fifteen minutes
+
+const proxyUsername = process.env.PROXY_USERNAME;
+const proxyPassword = process.env.PROXY_PASSWORD;
+const proxyHost = process.env.PROXY_HOST;
+const proxyPort = process.env.PROXY_PORT;
+const proxyAxios = axios.create({
+    proxy: false,
+    httpsAgent: createHttpsProxyAgent(`http://${proxyUsername}:${proxyPassword}@${proxyHost}:${proxyPort}`)
+});
 
 function delay(millis: number): Promise<void> {
     return new Promise(resolve => {
@@ -10,23 +21,7 @@ function delay(millis: number): Promise<void> {
     });
 }
 
-async function checkPledgeAvailability(config: PledgeConfig): Promise<boolean> {
-    try {
-        let result = await axios.get(config.projectHref);
-        let window = domino.createWindow(result.data);
-        let document = window.document;
-        let availablePledges = [...document.querySelectorAll('.sticky-rewards .mobile-hide ol > .pledge--available')]
-                                    .map(el => el.getAttribute('data-reward-id'));
-        return config.pledges.some(pledge => availablePledges.indexOf(pledge) !== -1);
-    }
-    catch (e) {
-        console.error(e);
-        return false;
-    }
-}
-
-async function sendAvailabilityNotification(config: PledgeConfig, isAvailable: boolean): Promise<void> {
-    let message = isAvailable ? `Pledge available! GO GET IT! ${config.projectHref}` : 'Whoops, too late. Try again later.';
+async function sendMessage(message: string) {
     console.log(message);
 
     let slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
@@ -38,6 +33,30 @@ async function sendAvailabilityNotification(config: PledgeConfig, isAvailable: b
         console.error('Failed to send message via slack.');
         console.error(e);
     }
+}
+
+async function checkPledgeAvailability(config: PledgeConfig): Promise<boolean> {
+    try {
+        console.log('Attempting to fetch pledge information...');
+        let result = await proxyAxios.get(config.projectHref);
+        console.log('Done.');
+        let window = domino.createWindow(result.data);
+        let document = window.document;
+        let availablePledges = [...document.querySelectorAll('.sticky-rewards .mobile-hide ol > .pledge--available')]
+                                    .map(el => el.getAttribute('data-reward-id'));
+        return config.pledges.some(pledge => availablePledges.indexOf(pledge) !== -1);
+    }
+    catch (e) {
+        console.error(e);
+        await sendMessage('Failed to fetch pledge information. Check logs, might have hit ratelimiter.');
+        await delay(BACKOFF_SLEEP_MILLIS);
+        return false;
+    }
+}
+
+async function sendAvailabilityNotification(config: PledgeConfig, isAvailable: boolean): Promise<void> {
+    let message = isAvailable ? `Pledge available! GO GET IT! ${config.projectHref}` : 'Whoops, too late. Try again later.';
+    await sendMessage(message);
 }
 
 async function main() {
